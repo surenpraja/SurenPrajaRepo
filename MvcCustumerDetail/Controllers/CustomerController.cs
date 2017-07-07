ec65134de78c10c54ec7f8fef9d356e6ad27c321
surenpraja <praja.sunny.31@gmail.com>
2017-07-07 16:14:48 -0500

    firt to Git

---
+++ MvcCustumerDetail/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCustumerDetail.Models;
+
+namespace MvcCustumerDetail.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult CustDetail(int id)
+        {
+            CustomerContext customerContext = new CustomerContext();
+            Customer cust =  customerContext.Customers.Single(x => x.EmployeeID == id);
+            
+            return View(cust);
+        }
+
+    }
+}
---
+++ MvcCustumerDetail/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcCustumerDetail.Models
+{
+    [Table("Employee")]
+    public class Customer
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}
